e220587a05de4b63c0167af58073144547ea08ea
Vishal Rajput <rajputvishal5748@outlook.com>
2022-02-13 13:47:18 +0530

    add: create ui of portfolio

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "glightbox/dist/js/glightbox.min.js"
+import "glightbox/src/js/glightbox.js"
+import "@srexi/purecounterjs/purecounter.js"
+import AOS from "aos";
+import "aos/dist/aos.css";
+App.AOS = new AOS.init({ disable: "phone" });
+import "swiper/swiper-bundle.js"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "boxicons/css/boxicons.min.css"
+import "swiper/swiper-bundle.min.css"
+import "glightbox/dist/css/glightbox.min.css"
+
 
 Vue.config.productionTip = false
 
